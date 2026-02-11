c2fc3ebde75f7aeb78c341a352ea761632f8816a
ridhi <kumariridhi1906i@gmail.com>
2026-02-11 21:42:29 +0530

    update

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
-# -----------------------------
 # 2. DATA GENERATION
 # -----------------------------
 # Synthetic dataset representing typing behavior
